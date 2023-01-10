a05a209c96c6f4786546d83eddee6bba66f41f6b
dkushlevich <119308472+dkushlevich@users.noreply.github.com>
2023-01-10 18:02:12 +0800

    Add files via upload

---
+++ products/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+from users.models import User
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveBigIntegerField(default=0)
+    image = models.ImageField(upload_to="product_images")
+    category = models.ForeignKey(ProductCategory, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"Продукт: {self.name} | Категория: {self.category}"
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = " Products"
+
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+    def __str__(self) -> str:
+        return f"Корзина для: {self.user.username} | Продукт: {self.product.name}"
+
+    def sum(self):
+        return self.product.price * self.quantity
